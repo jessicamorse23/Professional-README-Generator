09ed4e941a320f2c43a75d2a2d398e11d8478794
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-09-22 20:31:17 -0400

    fixed git ignore

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -36,7 +36,7 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.Title}
+  return `# ${data.title}
 
   ## Table of Contents
   *[Project Description](#description)
